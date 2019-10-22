b054c03ff6e1472f14c5177801d00589985e72a2
JWaltuch <jenna.waltuch@gmail.com>
2019-10-22 16:24:43 -0400

    all specs passing

--- mergesort.js
+++ mergesort.js
@@ -1,43 +1,53 @@
 function split(wholeArray) {
   //if the length is odd:
-  if(wholeArray.length===0||wholeArray.length===1){
+  if (wholeArray.length === 0 || wholeArray.length === 1) {
     return wholeArray;
-  }else{
-    const middle=wholeArray.length/2;
-    let firstHalf, secondHalf=[]
-    if(wholeArray.length%2===0){
-      firstHalf=wholeArray.slice(0,middle)
-      secondHalf=wholeArray.slice(middle)
-    }else{
-       firstHalf=wholeArray.slice(0,middle+1)
-       secondHalf=wholeArray.slice(middle+1)
+  } else {
+    const middle = wholeArray.length / 2;
+    let firstHalf,
+      secondHalf = [];
+    if (wholeArray.length % 2 === 0) {
+      firstHalf = wholeArray.slice(0, middle);
+      secondHalf = wholeArray.slice(middle);
+    } else {
+      firstHalf = wholeArray.slice(0, middle + 1);
+      secondHalf = wholeArray.slice(middle + 1);
     }
-  /* your code here to define the firstHalf and secondHalf arrays */
+    /* your code here to define the firstHalf and secondHalf arrays */
 
-  return [firstHalf, secondHalf];
+    return [firstHalf, secondHalf];
+  }
 }
-}
 
-
-function merge(firstArr, secondArr){
-
-  if(firstArr.length===0){
+function merge(firstArr, secondArr) {
+  if (firstArr.length === 0) {
     return secondArr;
-  }
-  else if(secondArr.length===0){
+  } else if (secondArr.length === 0) {
     return firstArr;
-  }
-  else{
-    let wholeArr=[];
-    if(firstArr[0]<secondArr[0]){
+  } else {
+    let wholeArr = [];
+    if (firstArr[0] < secondArr[0]) {
       wholeArr.push(firstArr[0]);
-      return wholeArr.concat(merge(firstArr.slice(1),secondArr))
+      return wholeArr.concat(merge(firstArr.slice(1), secondArr));
+    } else {
+      wholeArr.push(secondArr[0]);
+      return wholeArr.concat(merge(secondArr.slice(1), firstArr));
     }
-    else{
-      wholeArr.push(secondArr[0])
-      return wholeArr.concat(merge(secondArr.slice(1),firstArr))
-    }
-
   }
+}
 
+function mergeSort(array) {
+  if (array.length === 0 || array.length === 1) {
+    return array;
+  } else {
+    let splitArray = split(array);
+    let firstHalf = mergeSort(splitArray[0]);
+    let secondHalf = mergeSort(splitArray[1]);
+    return merge(firstHalf, secondHalf);
+  }
+  //split
+  //merge sort (first half)
+  //merge sort (second half)
+  //merge
+  /* your code here */
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -2,31 +2,49 @@ describe('Split Array function', function() {
   it('returns empty array if the array is empty', function() {
     expect(split([])).toEqual([]);
   });
-  it('returns an array itself if it has only one item', function(){
+  it('returns an array itself if it has only one item', function() {
     expect(split([5])).toEqual([5]);
-  })
+  });
   it('is able to split an array into two halves', function() {
-    expect(split([1,2,3,4]) ).toEqual( [[1,2], [3,4]] );
+    expect(split([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]]);
     // your code here
   });
-  it('if the number of items is odd, the first array will be bigger', function(){
-    expect(split([1,2,3,4,5])).toEqual([[1,2,3], [4,5]]);
-  })
+  it('if the number of items is odd, the first array will be bigger', function() {
+    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2, 3], [4, 5]]);
+  });
 });
 
+describe('Merge function', function() {
+  it('is able to merge two sorted arrays into one sorted array', function() {
+    expect(merge([3, 4], [1, 2])).toEqual([1, 2, 3, 4]);
+    expect(merge([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+  it('if one of the arrays empty, it returns non-empty array', function() {
+    expect(merge([], [5, 6])).toEqual([5, 6]);
+  });
+  it('return empty array if both arrays are empty', function() {
+    expect(merge([], [])).toEqual([]);
+  });
+  it('returns merged array if one array is bigger', function() {
+    expect(merge([2, 3, 4], [6, 7])).toEqual([2, 3, 4, 6, 7]);
+  });
+  it('should return sorted array even if some items are the same', function() {
+    expect(merge([1, 3], [1, 4])).toEqual([1, 1, 3, 4]);
+  });
+});
 
-describe('Merge function', function(){
-  it('is able to merge two sorted arrays into one sorted array', function(){
-    expect(merge([3,4],[1,2])).toEqual([1,2,3,4]);
-    expect(merge([1,2,3], [4,5,6])).toEqual([1,2,3,4,5,6]);
+describe('MergeSort function', function() {
+  it('is able to sort an array', function() {
+    expect(mergeSort([3, 1, 7, 2])).toEqual([1, 2, 3, 7]);
+    expect(mergeSort([9, 3, 1, 7, 2])).toEqual([1, 2, 3, 7, 9]);
   });
-  it('if one of the arrays empty, it returns non-empty array', function(){
-    expect(merge([], [5,6])).toEqual([5,6]);
+  it('should return empty array if empty', function() {
+    expect(mergeSort([])).toEqual([]);
   });
-  it('return empty array if both arrays are empty', function(){
-    expect(merge([],[])).toEqual([]);
+  it('should return single item array if only has one item', function() {
+    expect(mergeSort([1])).toEqual([1]);
   });
-  it('returns merged array if one array is bigger', function(){
-    expect(merge([2,3,4], [6,7])).toEqual([2,3,4,6,7]);
-  })
+  it('should return sorted array even if some items are the same', function() {
+    expect(mergeSort([1, 3, 1, 4])).toEqual([1, 1, 3, 4]);
+  });
 });
